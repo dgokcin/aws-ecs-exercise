5f5b766bbc12fabce3875e719cc297efbd0d2ce5
Deniz Gökçin <dgokcin@gmail.com>
2021-01-30 04:45:25 +0300

    added a hello message to the root url for debugging purposes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 """Main application file"""
+
 from flask import Flask
+
+MESSAGE = "Hello from Deniz!\n"
 app = Flask(__name__)
 
 @app.route('/<random_string>')
@@ -7,5 +10,11 @@ def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
     return "".join(reversed(random_string))
 
+@app.route("/")
+def root():
+    result = MESSAGE.encode("utf-8")
+    return result
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
